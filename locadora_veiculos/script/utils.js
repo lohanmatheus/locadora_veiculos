383f1a378921f37b71eb6449edb1d9e3e03cd4ce
lohanmatheus <lohan_cs@hotmail.com>
2021-11-11 14:18:04 -0300

    corrigindo utils.js

--- locadora_veiculos/script/utils.js
+++ locadora_veiculos/script/utils.js
@@ -195,30 +195,31 @@ function navbarSupportedContent(){
     }
 }
 
-$(document).ready(function (){
-$('#placa').mask('AAA 0U00', {
-    translation: {
-        'A': {
-            pattern: /[A-Za-z]/
+$(document).ready(function () {
+    $("#data-reserva, #data-entrega").mask("99/99/9999");
+    $('#placa').mask('AAA 0U00', {
+        translation: {
+            'A': {
+                pattern: /[A-Za-z]/
+            },
+            'U': {
+                pattern: /[A-Za-z0-9]/
+            },
         },
-        'U': {
-            pattern: /[A-Za-z0-9]/
-        },
-    },
-    onKeyPress: function (value, e, field, options) {
-        // Convert to uppercase
-        e.currentTarget.value = value.toUpperCase();
+        onKeyPress: function (value, e, field, options) {
+            // Convert to uppercase
+            e.currentTarget.value = value.toUpperCase();
 
-        // Get only valid characters
-        let val = value.replace(/[^\w]/g, '');
+            // Get only valid characters
+            let val = value.replace(/[^\w]/g, '');
 
-        // Detect plate format
-        let isNumeric = !isNaN(parseFloat(val[4])) && isFinite(val[4]);
-        let mask = 'AAA 0U00';
-        if(val.length > 4 && isNumeric) {
-            mask = 'AAA-0000';
+            // Detect plate format
+            let isNumeric = !isNaN(parseFloat(val[4])) && isFinite(val[4]);
+            let mask = 'AAA 0U00';
+            if (val.length > 4 && isNumeric) {
+                mask = 'AAA-0000';
+            }
+            $(field).mask(mask, options);
         }
-        $(field).mask(mask, options);
-    }
-});
+    });
 });
